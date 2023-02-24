9fcbcfae310bc404c66076e8ba8fca01b6877fe3
THiragi <w1th1nmybr41n@gmail.com>
2023-02-24 14:21:25 +0900

    add config $lib/config

---
+++ src/lib/config/site.ts
@@ -0,0 +1,10 @@
+export const site = {
+	name: 'interlinked',
+	description:
+		'趣味で描いた絵など置いています。現在は主に細胞BLACKの二次創作をしています。原作者様及び関係団体様等とは一切関係ありません',
+	url: 'https://interlinked.com',
+	owner: 'Isaka',
+	themeColor: '#AA2153',
+	mailTo: 'isakarokuro@gmail.com',
+	lang: 'ja'
+};
