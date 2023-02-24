ecfa999a8145857c95deb64ba3b469269c593624
THiragi <w1th1nmybr41n@gmail.com>
2023-02-24 18:11:15 +0900

    update config/site

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -1,5 +1,5 @@
 export const site = {
-	name: 'interlinked',
+	name: 'INTERLINKED',
 	description:
 		'趣味で描いた絵など置いています。現在は主に細胞BLACKの二次創作をしています。原作者様及び関係団体様等とは一切関係ありません',
 	url: 'https://interlinked.com',
