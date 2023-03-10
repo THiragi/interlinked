f64a433cf2eaadb37ff7a5698a17fd2226100a11
THiragi <w1th1nmybr41n@gmail.com>
2023-03-11 01:43:29 +0900

    commit

--- src/lib/components/BodyParser.svelte
+++ src/lib/components/BodyParser.svelte
@@ -2,9 +2,10 @@
 
 <script lang="ts">
 	import type { OgObject } from 'open-graph-scraper/lib/types';
-	import { Html, isTag } from 'html-svelte-parser';
+	import { Html, isTag, isText } from 'html-svelte-parser';
 	import type { Element, Text} from 'html-svelte-parser';
 	import CustomEmbedTweet from './CustomEmbedTweet.svelte';
+	import CustomLinkCard from './CustomLinkCard.svelte';
   import CustomYoutubeRenderer from './CustomYoutubeRenderer.svelte';
 	import ResponsiveImage from './ResponsiveImage.svelte';
 	export let html: string;
@@ -13,23 +14,35 @@
 	const includesTweet = html.includes('https://twitter.com/');
 
 	const processNode = (node: Element | Text) => {
-		if (isTag(node) && node.name === 'img') {
-			return {
-				component: ResponsiveImage,
-				props: { src: node.attribs.src, alt: node.attribs?.alt ?? '' }
-			};
-		}
-		if (isTag(node) && node.name === 'a' && node.attribs?.href?.includes('https://youtu.be/')) {
-			return {
-				component: CustomYoutubeRenderer,
-				props: { id: node.attribs.href.replace('https://youtu.be/', '') }
-			};
-		}
-		if (isTag(node) && node.name === 'a' && node.attribs?.href?.includes('https://twitter.com/')) {
-			return {
-				component: CustomEmbedTweet,
-				props: { href: node.attribs.href }
-			};
+		if (isTag(node)) {
+			if (node.name === 'img') {
+				return {
+					component: ResponsiveImage,
+					props: { src: node.attribs.src, alt: node.attribs?.alt ?? '' }
+				};
+			}
+			if (node.name === 'a') {
+				if (node.attribs?.href?.includes('https://youtu.be/')) {
+					return {
+						component: CustomYoutubeRenderer,
+						props: { id: node.attribs.href.replace('https://youtu.be/', '') }
+					};
+				}
+				if (node.attribs?.href?.includes('https://twitter.com/')) {
+					return {
+						component: CustomEmbedTweet,
+						props: { href: node.attribs.href }
+					};
+				}
+				if (!!node.firstChild && isText(node.firstChild) && node.firstChild.data === node.attribs?.href) {
+					const ogData = ogDatas.find((data) => data?.ogUrl === node.attribs?.href)
+					console.log(ogData)
+					return {
+						component: CustomLinkCard,
+						props: { ogData: ogData }
+					};
+				}
+			}
 		}
 	}
 
